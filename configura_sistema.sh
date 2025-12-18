2cfdc0b399a0253815e97c253dbe6276beeffdbf
Jutair Rios <84020383+jutair@users.noreply.github.com>
2025-12-18 08:16:33 -0400

    Update configura_sistema.sh

--- configura_sistema.sh
+++ configura_sistema.sh
@@ -5,12 +5,19 @@ echo Inslatando o comando sudo
 echo ######################################################################################################################
 apt install sudo -y
 echo ######################################################################################################################
+echo Criando os usuários
+echo sudo useradd -G sudo sudo -m jutair -s /bin/bash //Apenas em servidor VPS
+sudo usermod aG jutair
+sudo useradd -G sudo sudo -m guest -s /bin/bash
+sudo passwd guest
+echo ######################################################################################################################
 echo Atualizando o sistema...
 echo ######################################################################################################################
 sudo apt update && sudo apt upgrade -y
 echo ######################################################################################################################
 echo Instalando os pacote basicos de rede
 sudo apt install net-tools -y
+sudo apt install samba - y
 echo ######################################################################################################################
 echo Instalando os pacotes para SSH
 sudo apt install openssh-server -y
@@ -19,3 +26,16 @@ sudo systemctl enable ssh
 echo ######################################################################################################################
 echo Instalado os pacotes OpenVPN
 sudo apt install openvpn -y && sudo apt install easy-rsa -y
+echo ######################################################################################################################
+echo Fazendo backup das configurações básicas do sistema
+cd /home/jutair/Backup
+cp /etc/ssh/sshd_config /home/jutair/Backup
+cp /etc/samba/smb.conf /home/jutair/Backup
+echo ######################################################################################################################
+echo Baixando as configurações do OpenVPN
+wget https://raw.githubusercontent.com/Nyr/openvpn-install/master/openvpn-install.sh -O
+chmod +x openvpn-install.sh
+echo ######################################################################################################################
+echo Baixando as confiurações do SSH
+echo Baixando as confiurações do Samba
+echo Baixando os scripts de backup
