929a771843a2ae063f5065407c6ffa38336ed1db
Jutair Rios <84020383+jutair@users.noreply.github.com>
2025-12-19 06:08:49 -0400

    Update usuarios.sh

--- usuarios.sh
+++ usuarios.sh
@@ -1,11 +1,12 @@
+read -n 1 -p "Digite um caractere: " VARIAVEL
+# Exibindo o valor armazenado
+echo "Você digitou: '$VARIAVEL'"
 # Cria a pasta .ssh no novo usuário
-mkdir -p /home/jutair/.ssh
-
+#mkdir -p /home/jutair/.ssh
 # Copia as chaves autorizadas do root para o usuário
-cp /root/.ssh/authorized_keys /home/jutair/.ssh/
-
+#cp /root/.ssh/authorized_keys /home/jutair/.ssh/
 # Ajusta as permissões (MUITO IMPORTANTE)
 # O SSH não funciona se as permissões estiverem abertas demais
-chown -R jutair:jutair /home/jutair/.ssh
-chmod 700 /home/jutair/.ssh
-chmod 600 /home/jutair/.ssh/authorized_keys
+#chown -R jutair:jutair /home/jutair/.ssh
+#chmod 700 /home/jutair/.ssh
+#chmod 600 /home/jutair/.ssh/authorized_keys
