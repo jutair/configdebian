f99ed16b226a49467e39a64dfc5f6d172ef4e07a
Jutair Rios <84020383+jutair@users.noreply.github.com>
2025-12-21 19:28:53 -0400

    Add files via upload

---
+++ baixar.sh
@@ -0,0 +1,14 @@
+# Verifica se o script foi executado como root
+if [ "$EUID" -ne 0 ]; then
+  echo -e "\033[31mPor favor execute esse script como sudo!"
+  echo -e "\033[31msudo ./configura_sistema.sh"
+  echo -e "\033[0m"
+  exit 1
+fi
+wget https://github.com/jutair/configdebian/archive/refs/heads/main.zip
+unzip configdebian-main
+chmod +x configura_sistema.sh
+chmod +x menu.sh
+chmod +x open_vpn_conf.sh
+chmod +x usuarios.sh
+sudo ./menu.sh
