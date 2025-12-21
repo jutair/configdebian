44da7fe34a8d44e4cdbbcda5db3a5322318a21e4
Jutair Rios <84020383+jutair@users.noreply.github.com>
2025-12-21 19:35:46 -0400

    baixar.sh

--- baixar.sh
+++ baixar.sh
@@ -7,7 +7,7 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 wget https://github.com/jutair/configdebian/archive/refs/heads/main.zip
-unzip configdebian-main
+unzip main.zip
 cd /home/$USER/configdebian-main
 chmod +x configura_sistema.sh
 chmod +x menu.sh
