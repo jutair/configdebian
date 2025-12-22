963bc8d593257c7e6b952afafea8feb2b5976c12
Jutair Rios <84020383+jutair@users.noreply.github.com>
2025-12-21 20:24:32 -0400

    baixar.sh

--- baixar.sh
+++ baixar.sh
@@ -1,11 +1,15 @@
-# Verifica se o script foi executado como root
 if [ "$EUID" -ne 0 ]; then
-  echo -e "\033[31mPor favor execute esse script como sudo!"
-  echo -e "\033[31msudo ./configura_sistema.sh"
+  echo -e "\033[31mPor favor execute esse script como root!"
+  echo -e "\033[31mDe o comando su antes de executar esse script"
   echo -e "\033[0m"
   exit 1
 fi
-su
+# Pega o nome do primeiro usuário humano criado no sistema
+USUARIO_HUMANO=$(awk -F: '$3 >= 1000 && $3 != 65534 {print $1; exit}' /etc/passwd)
+# Pega a home desse usuário
+HOME_HUMANA=$(getent passwd "$USUARIO_HUMANO" | cut -d: -f6)
+echo "A pasta home detectada foi: $HOME_HUMANA"
+# Verifica se o script foi executado como root
 apt update && apt upgrade -y
 apt install sudo -y
 sudo apt install unzip -y
@@ -13,7 +17,7 @@ wget https://github.com/jutair/configdebian/archive/refs/heads/main.zip
 unzip main.zip
 sudo rm main.zip
 USER=${SUDO_USER:-$(whoami)}
-cd /home/$USER/configdebian-main
+cd /home/$HOME_HUMANA/configdebian-main
 chmod +x configura_sistema.sh
 chmod +x menu.sh
 chmod +x open_vpn_conf.sh
