15917b1d3f39ead2f6cb65a3ec4f44f510ab5c98
Jutair Rios <84020383+jutair@users.noreply.github.com>
2025-12-22 19:24:58 -0400

    menu.sh

--- menu.sh
+++ menu.sh
@@ -152,7 +152,7 @@ CURRENRT=$(logname 2>/dev/null || echo $SUDO_USER)
             [6]) ./usuarios.sh ;;
             [7]) update_sistema ;;
             [8]) ./open_vpn_conf.sh ;;
-            [9]) exit 0 ;;
+            [9]) exit ;;
             *) echo -e "\033[31mOpção inválida!\033[0m"; sleep 1 ;;
         esac
     done
