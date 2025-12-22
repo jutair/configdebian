21775ee27966e51a07c35b0232732a29f679b034
Jutair Rios <84020383+jutair@users.noreply.github.com>
2025-12-22 18:02:22 -0400

    open_vpn_conf.sh

--- open_vpn_conf.sh
+++ open_vpn_conf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-USER_ATUAL=${SUDO_USER:-$(whoami)}
+USER_ATUAL=$(logname)
 # Cores
 VERDE='\033[0;32m'
 VERMELHO='\033[31m'
@@ -23,7 +23,7 @@ STATUS_FILE="/etc/openvpn/server/openvpn-status.log"
 
 # --- FUNÇÕES ---
 veri_openvpn () {
-USER_ATUAL=${SUDO_USER:-$(whoami)}
+USER_ATUAL=$(logname)
     if ! command -v openvpn >/dev/null 2>&1; then
         echo -e "${AMARELO}[AVISO] OpenVPN não instalado.${SEM_COR}"
         sudo mkdir -p "/home/$USER_ATUAL/configdebian-main"
@@ -226,7 +226,7 @@ listar_ovpns() {
 ####################Função baixa ovpn############################################
 baixa() {
     IP_WAN=$(curl -s ifconfig.me)
-    USER_ATUAL=${SUDO_USER:-$(whoami)}
+    USER_ATUAL=$(logname)
     clear
     INDEX_FILE="/etc/openvpn/server/easy-rsa/pki/index.txt"
 
@@ -335,7 +335,7 @@ listar_usuarios() {
 ####################Fim da função listar usuários################################
 ###############Função para gerenciar usuários####################################
 user_gerencia() {
-USER_ATUAL=${SUDO_USER:-$(whoami)}
+USER_ATUAL=$(logname)
 # Caminho para o instalador do Angristan
 INSTALLER="/home/$USER_ATUAL/configdebian-main/openvpn-install.sh"
 
@@ -370,7 +370,7 @@ add_user() {
 
 # Função para Remover Usuário
 remove_user() {
-USER_ATUAL=${SUDO_USER:-$(whoami)}
+USER_ATUAL=$(logname)
     clear
     echo "======================================"
     echo "      REMOVER USUÁRIO EXISTENTE       "
@@ -429,7 +429,7 @@ done
 mover_ovp() {
     clear
     # Identifica o usuário real (mesmo usando sudo)
-    NOME_USUARIO=${SUDO_USER:-$(whoami)}
+    NOME_USUARIO=$(logname)
     DESTINO="/home/$NOME_USUARIO/clientes_ovp"
     # 1. Cria o destino e ajusta permissões da pasta
     if [ ! -d "$DESTINO" ]; then
@@ -474,7 +474,7 @@ mover_ovp() {
 atualiza_ovp() {
     clear
     # Identifica o usuário real (mesmo usando sudo)
-    NOME_USUARIO=${SUDO_USER:-$(whoami)}
+    NOME_USUARIO=$(logname)
     DESTINO="/home/$NOME_USUARIO/clientes_ovp"
     # 1. Cria o destino e ajusta permissões da pasta
     if [ ! -d "$DESTINO" ]; then
