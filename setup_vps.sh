97057cc316da8095b1a950049d56d132f84138f9
Jutair Rios <84020383+jutair@users.noreply.github.com>
2025-12-23 11:45:24 -0400

    setup_vps.sh

--- setup_vps.sh
+++ setup_vps.sh
@@ -1,22 +1,8 @@
-if [ "$EUID" -ne 0 ]; then
-  echo -e "\033[31mPor favor execute esse script como root!"
-  echo -e "\033[31mDe o comando su antes de executar esse script"
-  echo -e "\033[0m"
-  exit 1
-fi
-# Pega o nome do primeiro usuário humano criado no sistema
-USUARIO_HUMANO=$(awk -F: '$3 >= 1000 && $3 != 65534 {print $1; exit}' /etc/passwd)
-# Pega a home desse usuário
-HOME_HUMANA=$(getent passwd "$USUARIO_HUMANO" | cut -d: -f6)
-echo "A pasta home detectada foi: $HOME_HUMANA"
-# Verifica se o script foi executado como root
-apt update && apt upgrade -y
-apt install sudo -y
-sudo apt install unzip -y
-wget https://github.com/jutair/configdebian/archive/refs/heads/main.zip
-unzip main.zip
-sudo rm main.zip
-USER=${SUDO_USER:$(logname 2>/dev/null || echo $SUDO_USER)}
-cd $HOME_HUMANA/configdebian-main
-chmod +x configura_sistema.sh menu.sh open_vpn_conf.sh usuarios.sh update_sistema.sh gerencia_rede.sh
-exec sudo -E bash ./configura_sistema.sh
+#!/bin/bash
+cd /tmp
+wget -q https://github.com/jutair/configdebian/archive/refs/heads/main.zip
+unzip -o main.zip
+chmod +x configdebian-main/configura_sistema.sh
+./configdebian-main/configura_sistema.sh
+rm -rf /tmp/main.zip /tmp/configdebian-main
+rm -- "$0"  
